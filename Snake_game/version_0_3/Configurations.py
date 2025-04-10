5f85563e139f56dbd4bed01c0c35ef95e28c95e9
Scar-Galilea <floresmariana475@gmail.com>
2025-04-10 17:02:43 -0600

    Nuevas versiones

--- Snake_game/version_0_3/Configurations.py
+++ Snake_game/version_0_3/Configurations.py
@@ -7,6 +7,11 @@ class Configurations:
     _game_title = "Snake game en pygame"
     _background = (204, 169, 221)  # Fondo de la pantalla en formato RGB.
 
+    #Configuraciones de la serpiente.
+    _snake_block_size = 40
+    _snake_head_color = (255, 255, 255)
+    _snake_body_color = (0,255,0)
+
     @classmethod
     def get_creen_size(cls) -> tuple[int,int]:
         """
@@ -25,3 +30,24 @@ class Configurations:
         Getter para _background
         """
         return cls._background
+
+    @classmethod
+    def snake_block_size(cls) -> int:
+        """
+        Getter para _snake_block_size
+        """
+        return cls._snake_block_size
+
+    @classmethod
+    def snake_head_color(cls) -> tuple[int,int,int]:
+        """
+        Getter para _snake_head_color
+        """
+        return cls._snake_head_color
+
+    @classmethod
+    def snake_body_color(cls) -> tuple[int, int, int]:
+        """
+        Getter para _snake_body_color
+        """
+        return cls._snake_body_color
