0ba23c4e8ad373dce41d9db7841855ddeb7095ec
Scar-Galilea <floresmariana475@gmail.com>
2025-04-22 16:04:49 -0600

    Nuevas versiones

---
+++ Snake_game/version_0_7/Game_functionalities.py
@@ -0,0 +1,104 @@
+"""
+Nombre: Galilea Peralta Contreras.
+Fecha: 09 de abril del 2025.
+
+Descripción:
+"""
+import pygame
+from Configurations import Configurations
+from Snake  import  SnakeBlock
+from Manzana import Apple
+
+def game_event(snake_body: pygame.sprite.Group) -> bool:
+    """
+    Función que administra los eventos del juego
+    :return: La bandera del fin del juego
+    """
+
+    #Se declara la bandera del fin del juego
+    game_over = False
+
+    #Se verifican los eventos de
+    for event in pygame.event.get():
+        #Un clic en cerrar el juego
+        if event.type == pygame.QUIT:
+            game_over = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                SnakeBlock.set_is_moving_right(True)
+                SnakeBlock.set_is_moving_left(False)
+                SnakeBlock.set_is_moving_up(False)
+                SnakeBlock.set_is_moving_down(False)
+
+            if event.key == pygame.K_LEFT:
+                SnakeBlock.set_is_moving_right(False)
+                SnakeBlock.set_is_moving_left(True)
+                SnakeBlock.set_is_moving_up(False)
+                SnakeBlock.set_is_moving_down(False)
+
+            if event.key == pygame.K_UP:
+                SnakeBlock.set_is_moving_right(False)
+                SnakeBlock.set_is_moving_left(False)
+                SnakeBlock.set_is_moving_up(True)
+                SnakeBlock.set_is_moving_down(False)
+
+            if event.key == pygame.K_DOWN:
+                SnakeBlock.set_is_moving_right(False)
+                SnakeBlock.set_is_moving_left(False)
+                SnakeBlock.set_is_moving_up(False)
+                SnakeBlock.set_is_moving_down(True)
+
+            if event.key == pygame.K_g:
+                new_snake_block = SnakeBlock()
+                snake_body.add(new_snake_block)
+
+
+    #Se regresa la bandera
+    return game_over
+
+def snake_movement(snake_body: pygame.sprite.Group):
+    """
+    Funcion que gestiona el movimiento de cuerpo de la serpiente
+    :param snake_body: Grupo con el cuerpo de la serpiente:
+    """
+    body_size = len(snake_body.sprites()) -1
+
+    for i in range(body_size,0,-1):
+        snake_body.sprites()[i].rect.x = snake_body.sprites()[i-1].rect.x
+        snake_body.sprites()[i].rect.y = snake_body.sprites()[i - 1].rect.y
+
+    head = snake_body.sprites()[0]
+
+    if SnakeBlock.get_is_moving_right():
+        head.rect.x += Configurations.get_snake_block_size()
+    elif SnakeBlock.get_is_moving_left():
+        head.rect.x -= Configurations.get_snake_block_size()
+    elif SnakeBlock.get_is_moving_up():
+        head.rect.y -= Configurations.get_snake_block_size()
+    elif SnakeBlock.get_is_moving_down():
+        head.rect.y += Configurations.get_snake_block_size()
+
+
+def screen_refresh(screen: pygame.surface.Surface, clock: pygame.time.Clock, snake_body: pygame.sprite.Group,apple: Apple) -> None:
+    """
+    Función que administrar los elementos visuales del juego
+    """
+    #Fondo de la pantaña
+    screen.fill(Configurations.get_background())
+
+    #Se dibuja la cabeza de la serpiente
+    for snake_block in reversed(snake_body.sprites()):
+        snake_block.blit(screen)
+
+    #Se dibuja la manzana
+    apple.blit(screen)
+
+    pygame.display.flip()
+
+    #Se controla la velocidad de FPS
+    clock.tick(Configurations.get_fps())
+
+
+
+
