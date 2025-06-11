b48b63105ddc8c3965e13d9dcb69d9cfc5b4845e
Galilea Peralta Contreras <floresmariana475@gmail.com>
2025-06-11 09:29:14 -0600

    .

---
+++ Soldiers_vs_aliens/version_0_7/Game_functionalities.py
@@ -0,0 +1,70 @@
+"""
+Nombre: Galilea Peralta Contreras.
+Fecha: 09 de abril del 2025.
+
+Descripción:
+"""
+
+import  pygame
+from Configurations import Configurations
+from Media import Background
+from Soldiers import Soldiers
+from  Shot import Shot
+def game_event(soldiers: Soldiers, shot) -> bool:
+    #Se declara la bandera del fin del juego.
+    game_over = False
+
+    #Se verifican los eventos (teclado y ratón)
+    for event in pygame.event.get():
+
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                soldiers.is_moving_up = True
+            if event.key == pygame.K_DOWN:
+                soldiers.is_moving_down = True
+                soldiers.is_shot = True
+
+            if event.key == pygame.K_SPACE:
+                shotes = Shot(soldiers)
+                shot.add(shotes)
+                soldiers.shoots()
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                soldiers.is_moving_up = False
+
+            if event.key == pygame.K_DOWN:
+                soldiers.is_moving_down = False
+
+        #Un clic en cerrar el juego.
+        if event.type == pygame.QUIT:
+            game_over = True
+    #Se regresa la bandera
+    return game_over
+
+def screen_refresh(screen: pygame.surface.Surface,
+                   clock: pygame.time.Clock, background: Background, soldiers,shot) -> None:
+    """
+    Función que administra los elementos visuales del juego.
+    """
+    #Dibujamos la imagen de fondo en la pantalla.
+    background.blit(screen)
+    soldiers.update_position(screen)
+
+    soldiers.update_animation()
+    soldiers.blit(screen)
+
+    for shotes in reversed(shot.sprites()):
+        shotes.blit(screen)
+        shotes.update_animation()
+        shotes.update_position()
+
+    pygame.display.flip()  #Actualizamos el contenido de la ventana.
+
+    clock.tick(Configurations.get_fps())
+
+    soldiers.blit(screen)
+
+
+
